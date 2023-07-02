03cc9f220e0c95c9d6b65e2b5388fb33251a6c44
tronzed <106952970+tronzed@users.noreply.github.com>
2023-07-02 17:36:39 +0530

    many assets  files added

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+$('#banner_id').owlCarousel({
+
+    loop:true,
+
+    margin:10,
+
+    nav:0,
+
+    dots:0,
+
+    responsive:{
+
+        0:{
+
+            items:1
+
+        },
+
+        600:{
+
+            items:1
+
+        },
+
+        1000:{
+
+            items:1
+
+        }
+
+    }
+
+})
+
+
+AOS.init();
